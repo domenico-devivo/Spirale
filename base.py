a390de80b8e32c6bab349bb88b25eb915e94ac9f
domenico-devivo <dom.devivo@studenti.unina.it>
2023-01-18 23:46:29 +0100

    Terzo commit

--- base.py
+++ base.py
@@ -6,77 +6,101 @@ from code_pipeline.visualization import RoadTestVisualizer
 
 class RoadtitionBase():
     def __init__(self, executor=None, map_size=None):
-        self.executor = executor  # attributo che definisce il tipo di esecutore
-        self.map_size = map_size  # dimensione della mappa
-        self.visualize = RoadTestVisualizer(self.map_size)  # per visualizzare le strade a video
+        self.executor = executor  # attribute that defines the type of executor
+        self.map_size = map_size  # attribute specifying the square size of the map
+        self.visualize = RoadTestVisualizer(self.map_size)  # To display the roads on the screen
 
     def execute_test(self, road_points):
         '''
-        Metodo usato per generare test
+        Method used to generate tests
 
-        :param road_points: cartesian points of the road
+        :param road_points: list of road seen as cartesian points
         :return: the result of the test [Pass, Fail, Invalid or Error]
         '''
 
-        # trasformo la strada in un formato necessario per il testing
+        # I turn the road into a necessary format for testing
         test = RoadTestFactory.create_road_test(road_points)
-
-        # Con execute_tes si testa la strada precedentemente creata
+        # With execute_tes you test the previously created road
         test_outcome, description, execution_data = self.executor.execute_test(test)
         print("\033[1;34m test_outcome= \033[1;31m", test_outcome, "\033[1;30m")
+        #print("\033[1;34m execution_data.oob= \033[1;31m", execution_data[0].oob_distance, "\033[1;30m")
         self.visualize.visualize_road_test(test)
 
-        return test_outcome
+        return test_outcome , execution_data
 
-    def initial_population_generator(self, time_spent=1 / 4):
+    def initial_population_generator(self, n_max_of_road):
         '''
-        Genera la popolazione iniziale
+        Generates the initial population
 
-        :param time_spent: part of the total time that are spent for generate the starting population
+        :param n_max_of_road: max number of road generated for the starting population
 
         :return: list of road seen as initial population
         '''
         i = 0
-        strade_da_unire = []
+        road_population  = []
 
-        while self.executor.time_budget.get_remaining_real_time() > self.executor.time_budget.time_budget * (1 - time_spent) :
-            if (i % 2 == 0): #se i è pari allora genero un arco di ellisse in senso antiorario Sx <--
+        while i<n_max_of_road  :
+            if (i % 2 == 0): #If i is even then I generate an arc of spiral in counterclockwise direction Sx <--
                 road_points = self.bow(c_x0=randint(60, 120), c_y0=randint(60, 100), radius=randint(40, 70),
                                        interpolation_points=randint(4, 5), Angle_init=randint(295, 360),
                                        Angle_final=randint(180, 245))
 
-            else: # se i è dispari allora genero un arco di ellisse in senso orario --> Dx
+            else: # If i is odd then I generate an arc of spiral in clockwise direction --> Dx
                 road_points = self.bow(c_x0=randint(60, 120), c_y0=randint(60, 100), radius=randint(40, 70),
                                        interpolation_points=randint(4, 5), Angle_init=randint(0, 65),
                                        Angle_final=randint(115, 180))
 
-            test_outcome = self.execute_test(road_points)
+            x, y = self.roadpoint_to_xy(road_points)
+            x_reframe, y_reframe = self.reframe(x, y)
+            road_point_refremed = self.xy_to_roadpoint(x_reframe, y_reframe)
+            test_outcome , execution_data = self.execute_test(road_point_refremed)
 
-            if test_outcome != "ERROR":
-                strade_da_unire.append(road_points)
+            if test_outcome != "ERROR" and test_outcome != "INVALID":
+                road_population .append(road_point_refremed)
+                if test_outcome != "INVALID":
+                    print("\033[1;34m execution_data.oob= \033[1;31m", execution_data[0].oob_distance, "\033[1;30m")
+                    #IN BASE AL oob crea un vettore anche on gli oob a valore minore in execution_data e poi la funzione fittin la
+                    #fai andando a prendere il valore più piccolo della somma delle prime 5 tipo
 
             i = i + 1
 
-        return strade_da_unire
+        return road_population
 
-    def hebi_generator(self, strade_da_unire):
+    def hebi_generator(self, road_population):
 
-        for i in range(0, len(strade_da_unire)):
-            for j in range(0, len(strade_da_unire)):
+        heir_population=[]
+        oob=[]
+
+        for i in range(0, len(road_population)):
+            for j in range(0, len(road_population)):
 
                 if i != j:
-                    x1, y1 = self.roadpoint_to_xy(strade_da_unire[i])  # trasformo le prime 2 in coordinate x,y
-                    x2, y2 = self.roadpoint_to_xy(strade_da_unire[j])  # trasformo le prime 2 in coordinate x,y
-                    x_unione, y_unione = self.crossover(x1, y1, x2, y2)  # crossover tra le 2 strade
-                    x_reframe, y_reframe = self.reframe(x_unione, y_unione)
+                    x1, y1 = self.roadpoint_to_xy(road_population[i])  # I transform the first into x,y coordinates
+                    x2, y2 = self.roadpoint_to_xy(road_population[j])  # I transform the second into x,y coordinates
+                    x_union, y_union = self.crossover(x1, y1, x2, y2)  # crossover between the 2 roads
+                    x_reframe, y_reframe = self.reframe(x_union, y_union)
                     new_road_point = self.xy_to_roadpoint(x_reframe, y_reframe)
-                    self.execute_test(new_road_point)
-        return None
+                    test_outcome , execution_data = self.execute_test(new_road_point)
+                    if test_outcome != "ERROR" and test_outcome != "INVALID" :
+                        heir_population.append( new_road_point )
+                        oob.append(execution_data[0].oob_distance)
+                        print("\033[1;34m execution_data.oob= \033[1;31m", execution_data[0].oob_distance,"\033[1;30m")
+        return heir_population ,oob
 
     def bow(self, c_x0, c_y0, radius, interpolation_points, Angle_init, Angle_final):
-        # Generatore di strade modellate come archi di spirali
+        '''
+        Generator of roads modeled as spiral arcs
+
+        :param c_x0:
+        :param c_y0:
+        :param radius:
+        :param interpolation_points:
+        :param Angle_init:
+        :param Angle_final:
+        :return:
+        '''
         raggio_v = radius
-        random_coef= randint(3,4)
+        random_coef= randint(4,5)
         road_points = []
 
         center_x = c_x0
@@ -85,7 +109,7 @@ class RoadtitionBase():
         angles_in_deg = np.linspace(Angle_init, Angle_final, num=interpolation_points)
 
         for angle_in_rads in [math.radians(a) for a in angles_in_deg]:
-            raggio_v = raggio_v + random() * random_coef#  # RAGGIO_VARIABILE, potresti giocare con quel *2 e mettere un randint(iniziale,finale)
+            raggio_v = raggio_v + random() * random_coef
             x = math.sin(angle_in_rads) * raggio_v + center_x
             y = math.cos(angle_in_rads) * raggio_v + center_y
             road_points.append((x, y))
@@ -94,41 +118,65 @@ class RoadtitionBase():
         return road_points
 
     def crossover(self, x1, y1, x2, y2):
+        '''
+        crossover between the 2 roads
+
+        :param x1:
+        :param y1:
+        :param x2:
+        :param y2:
+        :return:
+        '''
         x_temp = []
-        x_diff = x2[0] - x1[-1]  # differenza tra il primo del secondo e l'ultimo del primo
+        x_diff = x2[0] - x1[-1]
         y_temp = []
         y_diff = y2[0] - y1[-1]
         for t in range(0, len(x2)):
-            x_temp.append(x2[t] - x_diff)  # vado a sottrarre il valore differenza per unire le 2 strade e crearne una sola
+            x_temp.append(x2[t] - x_diff)
 
-        for i in range(0, len(y2)):  # devo traslare tutti i valori di y2
-            y_temp.append(y2[i] - y_diff)  # vado a togliere la differenza di altezza tra i 2 valori, in modo da rendere la curva continua
+        for i in range(0, len(y2)):
+            y_temp.append(y2[i] - y_diff)
 
-        # quando unisco le due strade nel punto di congiunzione ci sono 2 punti consecutivi (x,y) uguali e il programma genera errore.
-        # quindi li elimino entrambi
         del x_temp[0]
         del y_temp[0]
 
-        del x1[-1]
-        del y1[-1]
+        #del x1[-1]
+        #del y1[-1]
 
-        x_unione = [*x1, *x_temp]  # unione delle ascisse
-        y_unione = [*y1, *y_temp]  # unione delle ordinate
+        x_unione = [*x1, *x_temp]
+        y_unione = [*y1, *y_temp]
 
         return x_unione, y_unione
 
     def xy_to_roadpoint(self, x, y):
+        '''
+        From 2 list of points to a list of
+        :param x:
+        :param y:
+        :return:
+        '''
         road_point = []
         for i in range(0, len(x)):
             road_point.append((x[i], y[i]))
         return road_point
 
     def roadpoint_to_xy(self, road_point):
+        '''
+
+        :param road_point:
+        :return:
+        '''
         x = [x for x, y in road_point]
         y = [y for x, y in road_point]
         return x, y
 
     def reframe(self, x_unione, y_unione):
+        '''
+
+        :param x_unione:
+        :param y_unione:
+        :return:
+        '''
         x_reframe = x_unione
         y_reframe = y_unione
 
@@ -163,13 +211,58 @@ class RoadtitionBase():
         return x_reframe, y_reframe
 
     def halve(self, x, y):
+        '''
 
-        slice_x = len(x) // 4           # voglio togliere 1/2 dei punti x, quindi:
-        del x[0:slice_x]                # tolgo 1/4 dei punti all'inizio
-        del x[len(x) - slice_x:len(x)]  # tolgo 1/4 dei punti alla fine
+        :param x:
+        :param y:
+        :return:
+        '''
 
-        slice_y = len(y) // 4           # stessa cosa faccio con l'ordiante
+        slice_x = len(x) // 4
+        del x[0:slice_x]
+        del x[len(x) - slice_x:len(x)]
+
+        slice_y = len(y) // 4
         del y[0:slice_y]
         del y[len(y) - slice_y:len(y)]
 
         return x, y
+
+
+
+    def fitness_function(self,heir_population , fitness_parent, oob):
+
+        v = True
+        while fitness_parent >= 0 and v:
+
+            if len(heir_population) > 4:
+                new_population , new_fitness = self.fitness_of_one_population(heir_population, oob , 5) #mi estrapolo solo le 5 strade che hanno fitness più basse, se ci sono!
+                #print("\nla nuova popolazione con oob minimo è:" , new_population," ed ha come funzione fitness:\033[1;31m",new_fitness,"\033[1;30m")
+
+                new_heir_population , oob=self.hebi_generator(new_population) #le incrocio
+
+                new_population[0:len(new_population)]=[] #azzero la popolazione per cacolarmene una nuova
+
+                fitness_parent = fitness_parent - new_fitness  # se il fitness di prima è più grande di quella nuova ok, continuo perché voglio diminuirla
+
+            else:
+                v = False
+                #print("Entro nell'else perché la popolazione non è abbastanza grande. Metto a False!")
+
+
+        return None
+
+    def fitness_of_one_population(self, heir_population , oob , n):
+
+        new_population = []
+        fitness = 0
+        oob_temp=oob[:]
+        for i in range(0, n):  # estraggo n valori dalla popolazione
+            minimo = min(oob_temp) # mi caolcolo la strada con oob minimo
+            fitness = fitness + minimo # faccio la sommatoria tra gli n minimi
+            indice_minimo = oob_temp.index(minimo)# mi ricavo l'indice
+            new_population.append(heir_population[indice_minimo]) # in modo da accodare la strada alla nuova popolazione con solo gli oob minori
+            del heir_population[indice_minimo] # e poi li elimino per non riprendere le stesse strade
+            del oob_temp[indice_minimo]
+
+        return new_population , fitness
--- main.py
+++ main.py
@@ -12,11 +12,23 @@ Road generator:
 
 class Roadtition(RoadtitionBase):
     def __init__(self, executor=None, map_size=None):
+        self.executor=executor
         super().__init__(executor=executor, map_size=map_size)
 
     def start(self):
 
-        initial_roads_population = self.initial_population_generator(time_spent=1 / 4)
-        self.hebi_generator(initial_roads_population)
+        while self.executor.time_budget.get_remaining_real_time() > 0:
+
+            print("\nSono nella popolazione iniziale")
+            initial_roads_population = self.initial_population_generator(n_max_of_road = 5)
+
+            print("\nComincio a fare il crossover")
+            heir_population_1 , oob = self.hebi_generator(initial_roads_population)
+            heir_population , fitness = self.fitness_of_one_population(heir_population_1 , oob, len(heir_population_1))
+            print("\nheir_population: ", heir_population)
+
+            print("\n--------------Entro nella fitness function-----------------------")
+            self.fitness_function(heir_population = heir_population ,fitness_parent = fitness, oob = oob)
 
         return None
+
