f4b6da0a24545d43afe65690d535442648f7dcfd
domenico-devivo <dom.devivo@studenti.unina.it>
2023-01-19 11:47:47 +0100

    Quinto commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from base import RoadtitionBase
 
-
 class Roadtition(RoadtitionBase):
     def __init__(self, executor=None, map_size=None):
         self.executor=executor
@@ -10,15 +9,15 @@ class Roadtition(RoadtitionBase):
 
         while self.executor.time_budget.get_remaining_real_time() > 0:
 
-            print("\nSono nella popolazione iniziale")
-            initial_roads_population = self.initial_population_generator(n_max_of_road = 5)
+            #print("\nSono nella popolazione iniziale")
+            initial_roads_population = self.initial_population_generator(n_max_of_road = 3)
 
-            print("\nComincio a fare il crossover")
+            #print("\nComincio a fare il crossover")
             heir_population_1 , oob = self.hebi_generator(initial_roads_population)
             heir_population , fitness = self.fitness_of_one_population(heir_population_1 , oob, len(heir_population_1))
-            print("\nheir_population: ", heir_population)
+            #print("\nheir_population: ", heir_population)
 
-            print("\n--------------Entro nella fitness function-----------------------")
+            #print("\n--------------Entro nella fitness function-----------------------")
             self.fitness_generator(heir_population = heir_population ,fitness_parent = fitness, oob = oob)
 
         return None
