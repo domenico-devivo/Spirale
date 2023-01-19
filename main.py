6f739b61cc82d96edb9a57e4b6f4b1dbe701865c
domenico-devivo <dom.devivo@studenti.unina.it>
2023-01-19 12:02:21 +0100

    Sesto commit

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from base import RoadtitionBase
+'''
+Road generator:
+    0)Iteratively: until the time budget is not over
+     1)A initial population is generated, seen as a set of spiral arcs
+     2)A heir population is generated ,joining the arcs with the Cartesian product between the inital set with itself ( excluding equal indices )
+     3)A fitness value is associated to the heir population
+     4)A new heir population is derived as a subset of the first one, and also valued with the fitness value
+     5)The two fitness value are compared and IF the fitness of the first is minus of the new one than goes to the --> 1)
+        6)else goes to the ----> 4)
 
+    i.e: 
+    1) initial population=[S1, S2, S3]
+    2) heir population =[S1US2, S1US3, S2US1, S2US3, S3US1, S3US2] with fitness value = 6
+    3) derived heir population =[S1US2, S2US3]  with fitness value = 2
+    4) 6>2   --> new heir population =[(S1US2)U(S2US3), (S2US3)U(S1US2)] with fitness value = 3
+    5) 2<3>  ----> 1) a new initial population is generated
+'''
 
 class Roadtition(RoadtitionBase):
     def __init__(self, executor=None, map_size=None):
